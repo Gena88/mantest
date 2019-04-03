a3baaeffeff88fad5047611b2055ab5ac78a17ad
Gena88 <32675403+Gena88@users.noreply.github.com>
2019-04-03 17:41:54 +0300

    рефакторинг

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -3,4 +3,8 @@
 def test__del_project(app):
     # if app.project.count() == 0:
     #     app.project.create()
+    old_project = app.project.get_project_list()
     app.project.delete_first_project()
+    new_project = app.project.get_project_list()
+    assert len(old_project) - 1 == len(new_project)
+
--- test/test_modification_project.py
+++ test/test_modification_project.py
@@ -1,4 +1,7 @@
 
 
 def test_modification_project(app):
+    old_project = app.project.get_project_list()
     app.project.modification()
+    new_project = app.project.get_project_list()
+    assert len(old_project) == len(new_project)
